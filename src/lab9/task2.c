c4fef826c9b632e9d3437bc9dd045b9d7086b3b7
heimoshuiyu <heimoshuiyu@gmail.com>
2020-11-12 20:49:44 +0800

    lab9 add comment

--- src/lab9/task2.c
+++ src/lab9/task2.c
@@ -11,22 +11,33 @@
 
 #include <stdio.h>
 
+/* return the highest number in array a[],
+ * array size should be n */
 float get_highest(float a[], int n);
 
 int main() {
 	float value[4] = {2.5, 1, 1.2, 5.2};
+
+	/* get the highest number in value[4] */
 	float highest = get_highest(value, 4);
+
+	/* outpu the highest number */
 	printf("highest number is %.1f\n", highest);
+
 	return 0;
 }
 
 float get_highest(float a[], int n) {
+
+	/* take first number */
 	float highest = a[0];
+
+	/* loop to find the highest number */
 	for (int i=0; i<n; i++) {
 		if (highest < a[i]) {
 			highest = a[i];
 		}
 	}
+
 	return highest;
 }
-	
