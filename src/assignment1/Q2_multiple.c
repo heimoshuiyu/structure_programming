951a968b4aa6b916a37d94f25c15d755f2fe0ef3
heimoshuiyu <heimoshuiyu@gmail.com>
2020-11-22 18:07:06 +0800

    assignment_1 Q2 update

--- src/assignment1/Q2_multiple.c
+++ src/assignment1/Q2_multiple.c
@@ -8,6 +8,7 @@
 
 int main() {
 	int input;
+	int result = 0;
 
 	/* input a integer */
 	printf("Input a integer: ");
@@ -15,12 +16,23 @@ int main() {
 
 	/* check if mutiple of 3 */
 	if (input % 3 == 0) {
-		printf("%d is mutiple of 3\n", input);
+		result += 3;
 	}
 
 	/* check if mutiple of 2 */
 	if (input % 2 == 0) {
-		printf("%d is mutiple of 2\n", input);
+		result += 2;
+	}
+
+	/* show result */
+	switch (result) {
+		case 0: printf("%d - is not a multiple of 2 or 3\n", input); break;
+		case 2: printf("%d - is a multiple of 2\n", input); break;
+		case 3: printf("%d - is a multiple of 3\n", input); break;
+		case 5: printf("%d - is a multiple of 2 and 3\n", input); break;
+		default:
+			printf("Error, result is %d\n", result);
+			return 1;
 	}
 
 	return 0;
