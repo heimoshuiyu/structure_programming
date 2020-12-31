18db62dc2b607e3770b406b18dfbd110b1fb3d31
heimoshuiyu <heimoshuiyu@gmail.com>
2020-12-31 16:06:29 +0800

    group project change structure

--- src/project/main.c
+++ src/project/main.c
@@ -1,39 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
-void function_list();
-void Import() {
-	printf("Here is the Import function\n");
-	getchar();
-	getchar();
-	/*
-	   You write code here about the Import
-	   */
-}
-void Delete() {
-	printf("Here is the Delete function\n");
-	getchar();
-	getchar();
 
-	/*
-	   You write code here about the Delete
-	   */
-}
-void Calculate() {
-	printf("Here is the Calculate function\n");
-	getchar();
-	getchar();
-	/*
-	   You write code here about the Calculate
-	   */
-}
-void Export() {
-	printf("Here is the Export function\n");
-	getchar();
-	getchar();
-	/*
-	   You write code here about the Export
-	   */
-}
+void function_list();
 void Quit() { exit(0); }
 
 int main() {
@@ -50,15 +18,18 @@ void function_list() {
 
 		printf("\t\t|###############################################|\n");
 		printf("\t\t|                                               |\n");
-		printf("\t\t|    Welcome to Use Grade management System     |\n");
+		printf("\t\t|    Welcome to Use My Personal note book       |\n");
 		printf("\t\t|                                               |\n");
 		printf("\t\t|   Please Select The Following Functions:      |\n");
 		printf("\t\t|                                               |\n");
-		printf("\t\t|   $$ 1 -Import grades for a course            |\n");
-		printf("\t\t|   $$ 2 -Delete a course                       |\n");
-		printf("\t\t|   $$ 3 -Calculate GPA                         |\n");
-		printf("\t\t|   $$ 4 -Export file with GPA                  |\n");
-		printf("\t\t|   $$ 5 -Quit                                  |\n");
+		printf("\t\t|   $$ 1 -Add Person                            |\n");
+		printf("\t\t|   $$ 2 -Remove Person                         |\n");
+		printf("\t\t|   $$ 3 -Update Person                         |\n");
+		printf("\t\t|   $$ 4 -Find and View Person details          |\n");
+		printf("\t\t|   $$ 5 -All female / male                     |\n");
+		printf("\t\t|   $$ 6 -List by name A-z / Z-a                |\n");
+		printf("\t\t|   $$ 7 -By age                                |\n");
+		printf("\t\t|   $$ 8 -Quit -                                |\n");
 		printf("\t\t|                                               |\n");
 		printf("\t\t|###############################################|\n");
 
@@ -70,22 +41,24 @@ void function_list() {
 		switch (i) {
 
 			case 1:
-				Import();
 				break;
 			case 2:
-				Delete();
 				break;
 			case 3:
-				Calculate();
 				break;
 			case 4:
-				Export();
 				break;
 			case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
 				Quit();
 				break;
 			default:
-				printf("Number should between 1 -- 5!\n");
+				printf("Number should between 1 -- 8!\n");
 		}
 
 	} while (1);
